a19a9551c0f1e37d8c54fab62ff79cbdc81892be
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-02 17:11:27 -0500

    mostly working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,38 +3,39 @@
 //character objects: each has 3 attributes: `Health Points`, `Attack Power` and `Counter Attack Power` Player's attack power is increased by their base amount with each attack. Player uses `Attack Power` and enemy uses `Counter Attack Power`.
 
 $(document).ready(function () {
-    var theFighter;
-    var theCurrentEnemy;
+    var theFighter = "";
+    var theCurrentEnemy = "";
     var clickCheckString = "";
+
     characters = {
         luke: {
             name: "Luke Skywalker",
-            healthPoints: 0,
-            attackPower: 0,
+            healthPoints: 110,
+            attackPower: 20,
             counterAttackPower: 0,
             winningPhrase: "\"I’ll never turn to the dark side.\"",
             losingPhrase: "\"I have a very bad feeling about this.\"",
         },
         vader: {
             name: "Darth Vader",
-            healthPoints: 0,
-            attackPower: 0,
+            healthPoints: 160,
+            attackPower: 50,
             counterAttackPower: 0,
             winningPhrase: "\"Your powers are weak.\"",
             losingPhrase: "\"The Force is strong with this one.\"",
         },
         stormtrooper: {
             name: "Stormtrooper",
-            healthPoints: 0,
-            attackPower: 0,
+            healthPoints: 80,
+            attackPower: 15,
             counterAttackPower: 0,
             winningPhrase: "\"Inform Lord Vader we have prisoners.\"",
             losingPhrase: "\"These aren't the droids we're looking for.\"",
         },
         compactor: {
             name: "Trash Compactor",
-            healthPoints: 0,
-            attackPower: 0,
+            healthPoints: 60,
+            attackPower: 10,
             counterAttackPower: 0,
             winningPhrase: "[nom nom nom]",
             losingPhrase: "[sputter pffft]",
@@ -86,6 +87,26 @@ $(document).ready(function () {
 
     };
 
+    function doAttack() {
+        let theFighterGrabString = eval("characters." + theFighter);
+        let theCurrentEnemyGrabString = eval("characters." + theCurrentEnemy);
+        // fighter's health = health - enemy's attack power
+        theFighterGrabString.healthPoints = theFighterGrabString.healthPoints - theCurrentEnemyGrabString.attackPower;
+        // fighter's counter attack power = counter attack power + attack power each round
+        theFighterGrabString.counterAttackPower = theFighterGrabString.counterAttackPower + theFighterGrabString.attackPower;
+        // enemy's health = health - theFighter's counter attack power
+        theCurrentEnemyGrabString.healthPoints = theCurrentEnemyGrabString.healthPoints - theFighterGrabString.counterAttackPower;
+        console.log("Your health points are: " + theFighterGrabString.healthPoints);
+        console.log("Your attack power has grown to :" + theFighterGrabString.counterAttackPower + " per attack");
+        console.log("Your enemy's health points are: " + theCurrentEnemyGrabString.healthPoints);
+        // update stats
+        let theToolTipText = assembleToolTipText(theFighter);
+        $("#" + theFighter + " > span").text(theToolTipText);
+        theToolTipText = assembleToolTipText(theCurrentEnemy);
+        $("#" + theCurrentEnemy + " > span").text(theToolTipText);
+
+    };
+
     $("#display").click(function (event) {
         // only accept clicks on character avatars
         if (clickCheckString.includes(event.target.id) && event.target.id !== "") {
@@ -99,17 +120,21 @@ $(document).ready(function () {
                 $("#display > div").attr({ "class": "attack display-character tooltip" });
             } else {
                 if ($("#heading").text() == "Click your fighter to attack!") {
-                    //do some attacking
-                    if (1 === 1) { //if you win
+                    doAttack();
+                    if (eval("characters." + theCurrentEnemy).healthPoints < 1) { //if you win
                         clearTheEnemy(); //clears the enemy and the game continues
-                    } else {
-                        //do a game-over thing
+                    } else { //if you lose
+                        if (eval("characters." + theFighter).healthPoints < 1) {
+                            setTimeout(function () {
+                                alert("You lose!");
+                            }, 100);
+                        }
                     };
                 };
             };
             if ($("#heading").text() == "Choose your fighter") {
-                theFighter = event.target.id
-                resetChooseEnemy()
+                theFighter = event.target.id;
+                resetChooseEnemy();
                 $("#heading").text("Choose an enemy to fight");
                 $("#display > div").attr({ "class": "choose-enemy display-character tooltip" });
             };
@@ -149,16 +174,15 @@ $(document).ready(function () {
             $("#defeated-enemies-heading").text("Defeated enemies").attr({ "style": "opacity: 0" });
             $("#defeated-enemies-heading").animate({ opacity: "1" }, 1500);
         };
-
         if ($("#defeated-enemies > div").length === 3) {//if all the enemies have been defeated then
             $("#heading").html("&nbsp;");
             setTimeout(function () {
                 updateSectionWithFadeIn(theFighter, "#display", "replace");
-                $("#heading").html("<em>You have defeated all the enemies!</em>");
+                $("#heading").html("<em>You have defeated all the enemies!</em>").attr({ "style": "opacity: 0" });
+                $("#heading").animate({ opacity: "1" }, 1500);
                 theItemToAppend = ($("<span>").attr({ "class": "display-final-quote", "style": "opacity: 0" }).html(eval("characters." + theFighter + ".winningPhrase")));
                 $("#display").append(theItemToAppend);
                 eval(theItemToAppend).animate({ opacity: "1" }, 1500);
-
             }, 1000);
         } else {
             setTimeout(function () {
