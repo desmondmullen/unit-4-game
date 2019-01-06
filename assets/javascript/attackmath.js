80a47475f1c5ffd592e3efe6fce1e4d5ec121782
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-06 16:34:06 -0500

    beta 7.7 with attackmath files

--- assets/javascript/attackmath.js
+++ assets/javascript/attackmath.js
@@ -38,6 +38,10 @@ $(document).ready(function () {
         }
     };
 
+    var theFighterHealth = 0;
+    var theFighterAttack = 0;
+    var theEnemyHealth = 0;
+
     // if (window.matchMedia("(max-width: 670px)").matches) {
     // };
 
@@ -48,7 +52,7 @@ $(document).ready(function () {
     function initializePage() {
         if (typeof characters != "undefined") {
             for (let theCount = 0; theCount < Object.keys(characters).length; theCount++) {
-                let theCharacter = Object.keys(characters)[theCount]
+                let theCharacter = Object.keys(characters)[theCount];
                 initializeFields(theCharacter, theCount);
             };
         };
@@ -61,32 +65,50 @@ $(document).ready(function () {
     };
 
     function runOneRound(x, y) {
-        let theNumber = 0;
-        console.log("fighter's (" + x + ") hp: " + theNumber)
-        console.log("fighter's (" + x + ") health: " + theNumber)
-        console.log("enemy's (" + y + ") hp: " + theNumber)
+        let theFighter = Object.keys(characters)[x - 1];
+        let theEnemy = Object.keys(characters)[y - 1];
         // subtract enemy's attack from fighter's hp
-        // subtract fighter's attack from enemy hp
+        let theEnemyAttack = eval("characters." + theEnemy + ".attackPower")
+        theFighterHealth = theFighterHealth - theEnemyAttack
         // set fighter's attack to itself plus the base
+
+        theFighterAttack = theFighterAttack + eval("characters." + theFighter + ".attackPower");
+        // subtract fighter's attack from enemy hp
+        // console.log(theEnemyHealth + " / " + theFighterAttack + " / " + (theEnemyHealth - theFighterAttack));
+        theEnemyHealth = theEnemyHealth - theFighterAttack
+        console.log("enemy's attack: " + eval("characters." + theEnemy + ".attackPower"));
+        console.log("fighter's post-fight hp: " + theFighterHealth);
+        console.log("enemy's initial hp: " + eval("characters." + theEnemy + ".healthPoints"));
+        console.log("fighter's attack: " + theFighterAttack);
+        console.log("enemy's new hp: " + theEnemyHealth);
+        console.log("--------------------------");
+        return theFighterHealth;
     };
 
     function runThisScenario() {
         let theScenario = "1234"; // the first of a list of scenarios
         let theFighter = theScenario.substring(0, 1); // fighter is the first character of theScenario
         theScenario = theScenario.substring(1); // delete the first character of theScenario
+        theFighterHealth = eval("characters." + (Object.keys(characters)[theFighter - 1]
+            + ".healthPoints"));
+        theFighterAttack = eval("characters." + (Object.keys(characters)[theFighter - 1]
+            + ".attackPower"));
+        // console.log("check: " + theFighterAttack);
+        console.log("fighter's initial hp: " + theFighterHealth);
+        console.log("fighter's initial attack: " + theFighterAttack);
+        console.log("*****************************");
         for (let n = 0; n < 3; n++) { // there are 3 enemies
             let theEnemy = theScenario.substring(0, 1); // enemy is the first character of theScenario
+            theEnemyHealth = eval("characters." + (Object.keys(characters)[theEnemy - 1] + ".healthPoints"));
+            // console.log("enemy's initial hp: " + theEnemyHealth);
+            // console.log(Object.keys(characters)[theEnemy - 1] + "'s initial health: " + theEnemyHealth);
             theScenario = theScenario.substring(1); // delete the first character of theScenario
-            // console.log("the fighter: " + theFighter);
-            // console.log("the enemy: " + theEnemy);
-            // console.log("the remaining scenario: " + theScenario);
             let enemyStillAlive = true;
             while (enemyStillAlive) { // while enemyStillAlive is true
                 runOneRound(theFighter, theEnemy);
                 // if enemy's health < 1 then set enemyStillAlive to false
                 enemyStillAlive = false;
             }
-            // delete the first number of theScenario
         }
         // record whether the fighter and/or the enemy are still alive
     }
