d92af8e971f0fe9a4ee48a9c4481bfe2e223845b
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-06 19:55:24 -0500

    beta 7.7 with attackmath files

--- assets/javascript/attackmath.js
+++ assets/javascript/attackmath.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
         }
     };
 
+    scenarios = [1234, 1243, 1324, 1342, 1423, 1432, 2134, 2143, 2314, 2341, 2413, 2431, 3124, 3142, 3214, 3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321];
+
     var theFighterHealth = 0;
     var theFighterAttack = 0;
     var theEnemyHealth = 0;
@@ -72,30 +74,33 @@ $(document).ready(function () {
         console.log("enemy " + y + "'s pre-fight hp: " + thePreFightEnemyHealth);
         console.log("fighter's attack: " + theFighterAttack);
         console.log("enemy " + y + "'s post-fight hp: " + theEnemyHealth);
-        console.log("<br>--------------------------");
+        console.log(" ");
         $("#results").append("<br>enemy " + y + "'s attack: " + theEnemyAttack);
         $("#results").append("<br>fighter's post-fight hp: " + theFighterHealth);
         $("#results").append("<br>enemy " + y + "'s pre-fight hp: " + thePreFightEnemyHealth);
         $("#results").append("<br>fighter's attack: " + theFighterAttack);
         $("#results").append("<br>enemy " + y + "'s post-fight hp: " + theEnemyHealth);
-        $("#results").append("<br>--------------------------");
-        // return theFighterHealth;
+        $("#results").append("<br>***************************<br>");
     };
 
     function runThisScenario() {
         loadDataIntoVariables()
-        setTimeout(function () {
-            let theScenario = "1234"; // the first of a list of scenarios
-            let theFighter = parseInt(theScenario.substring(0, 1)); // fighter is the first character of theScenario
+        for (let scenarioCount = 0; scenarioCount < scenarios.length; scenarioCount++) {
+            let theScenario = (scenarios[scenarioCount]).toString(); // get the scenario
+            let theScenarioTitle = theScenario;
+            scenarios[scenarioCount].shift// delete that scenario from the array
+            let theFighter = parseInt(theScenario.substring(0, 1)); // the fighter is the first char of the scenario
             theScenario = theScenario.substring(1); // delete the first character of theScenario
             theFighterHealth = eval("characters." + Object.keys(characters)[theFighter - 1]).health;
             theFighterAttack = eval("characters." + Object.keys(characters)[theFighter - 1]).attack;
+            console.log("=== Scenario " + theScenarioTitle + "===");
             console.log("fighter's initial hp: " + theFighterHealth);
             console.log("fighter's initial attack: " + theFighterAttack);
-            console.log("*****************************");
-            $("#results").html("fighter's initial hp: " + theFighterHealth);
+            console.log(" ");
+            $("#results").append("<br> ====== Scenario " + theScenarioTitle + " ======");
+            $("#results").append("<br>fighter's initial hp: " + theFighterHealth);
             $("#results").append("<br>fighter's initial attack: " + theFighterAttack);
-            $("#results").append("<br>*****************************");
+            $("#results").append("<br>");
             for (let n = 0; n < 3; n++) { // there are 3 enemies
                 let theEnemy = theScenario.substring(0, 1); // enemy is the first character of theScenario
                 theEnemyHealth = eval("characters." + (Object.keys(characters)[theEnemy - 1] + ".health"));
@@ -106,7 +111,7 @@ $(document).ready(function () {
                     runOneRound(theFighter, theEnemy);
                 }
             }
-        }, 500);
+        }
     }
 
     $("#run-scenarios").click(function (event) {
