972322df4e5c0ba11aed211f473126d6373d4dc5
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-06 23:05:33 -0500

    beta 7.7 with attackmath files

--- assets/javascript/attackmath.js
+++ assets/javascript/attackmath.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
     var theFighterAttack = 0;
     var theEnemyHealth = 0;
     var theFighterInitialAttack = 0;
+    var thePreFightEnemyHealth = 0;
 
     function resetPage() {
         location = location;
@@ -42,35 +43,38 @@ $(document).ready(function () {
         var player4attack = document.getElementById('player4-attack').value;
         eval(characters.one).name = player1name;
         eval(characters.one).health = player1health;
+        eval(characters.one).preFightHealth = player1health;
         eval(characters.one).attack = player1attack;
         eval(characters.two).name = player2name;
         eval(characters.two).health = player2health;
+        eval(characters.two).preFightHealth = player2health;
         eval(characters.two).attack = player2attack;
         eval(characters.three).name = player3name;
         eval(characters.three).health = player3health;
+        eval(characters.three).preFightHealth = player3health;
         eval(characters.three).attack = player3attack;
         eval(characters.four).name = player4name;
         eval(characters.four).health = player4health;
+        eval(characters.four).preFightHealth = player4health;
         eval(characters.four).attack = player4attack;
     };
 
-    function runOneRound(x, y) {
+    function runOneRound(x, y, z) {
         let theFighter = Object.keys(characters)[x - 1];
         let theEnemy = Object.keys(characters)[y - 1];
         // subtract enemy's attack from fighter's hp
         let theEnemyAttack = (parseInt(eval("characters." + theEnemy + ".attack")));
         thePreFightFighterHealth = theFighterHealth;
-        theFighterHealth = theFighterHealth - theEnemyAttack;
+        theFighterHealth = parseInt(theFighterHealth) - parseInt(theEnemyAttack);
         // set fighter's attack to itself plus the base
         thePreFightFighterAttack = parseInt(theFighterAttack);
         theFighterAttack = (parseInt(theFighterAttack) + parseInt(theFighterInitialAttack));
         // subtract fighter's attack from enemy hp
-        thePreFightEnemyHealth = theEnemyHealth;
         theEnemyHealth = parseInt(theEnemyHealth) - parseInt(thePreFightFighterAttack) //post-fight
         console.log("fighter's pre-fight hp: " + thePreFightFighterHealth);
         console.log("enemy " + y + "'s attack: " + theEnemyAttack);
         console.log("fighter's post-fight hp: " + theFighterHealth);
-        console.log("enemy " + y + "'s pre-fight hp: " + thePreFightEnemyHealth);
+        console.log("enemy " + y + "'s pre-fight hp: " + z);
         console.log("fighter's attack: " + thePreFightFighterAttack);
         console.log("enemy " + y + "'s post-fight hp: " + theEnemyHealth);
         console.log("fighter's post-fight attack: " + theFighterAttack);
@@ -97,12 +101,16 @@ $(document).ready(function () {
             scenarios[scenarioCount].shift// delete that scenario from the array
             let theFighter = parseInt(theScenario.substring(0, 1)); // the fighter is the first char of the scenario
             theScenario = theScenario.substring(1); // delete the first character of theScenario
+            // console.log("scenario count: " + scenarioCount);
             if (scenarioCount === 0) {
                 theFighterHealth = eval("characters." + Object.keys(characters)[theFighter - 1]).health;
                 theFighterInitialHealth = theFighterHealth;
                 theFighterAttack = eval("characters." + Object.keys(characters)[theFighter - 1]).attack;
                 theFighterInitialAttack = theFighterAttack;
-            }
+            } else {
+                theFighterHealth = theFighterInitialHealth;
+                theFighterAttack = theFighterInitialAttack;
+            };
             console.log("=== Scenario " + theScenarioTitle + "===");
             console.log("fighter's initial hp: " + theFighterInitialHealth);
             console.log("fighter's initial attack: " + theFighterInitialAttack);
@@ -115,10 +123,11 @@ $(document).ready(function () {
                 console.log(" >> round " + n + " <<");
                 $("#results").append("<br> ---- round " + n + " ----");
                 let theEnemy = theScenario.substring(0, 1); // enemy is the first character of theScenario
-                theEnemyHealth = eval("characters." + (Object.keys(characters)[theEnemy - 1] + ".health"));
+                thePreFightEnemyHealth = (parseInt(eval("characters." + Object.keys(characters)[theEnemy - 1] + ".preFightHealth")));
+                theEnemyHealth = thePreFightEnemyHealth
                 theScenario = theScenario.substring(1); // delete the first character of theScenario
                 while (theEnemyHealth > 0) { // while enemyStillAlive is true
-                    runOneRound(theFighter, theEnemy);
+                    runOneRound(theFighter, theEnemy, theEnemyHealth);
                 }
             }
         }
